76a3a6c91cd7a9db222ea0fbfa5cf1e0def5cd07
DaniilLogvinenko <daniillogvinenkospb@mail.ru>
2023-12-10 22:07:27 +0300

    multiple themes

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -3,13 +3,25 @@ import { Splitter } from "primereact/splitter";
 import { NavLink } from "react-router-dom";
 import { InputSwitch } from "primereact/inputswitch";
 import { useState } from "react";
+import { Dropdown, DropdownChangeEvent } from "primereact/dropdown";
 
 enum Themes {
     dark = "/themes/arya-green/theme.css",
     light = "/themes/lara-light-cyan/theme.css",
 }
 
-const changeTheme = (theme: Themes) => {
+const DropdownThemes = [
+    "lara-light-cyan",
+    "arya-green",
+    "soho-dark",
+    "viva-light",
+    "viva-dark",
+    "vela-blue",
+    "bootstrap4-light-blue",
+    "bootstrap4-dark-blue",
+];
+
+const changeTheme = (theme: string) => {
     let themeLink = document.getElementById("app-theme") as HTMLAnchorElement;
     if (themeLink) {
         themeLink.href = theme;
@@ -20,10 +32,17 @@ export const Header = () => {
     const [theme, setTheme] = useState(Themes.light);
     let isLight = theme === Themes.light;
 
+    const [dropdowntheme, setDropdowntheme] = useState("lara-light-cyan");
+
     const handleOnThemeChange = () => {
         const newTheme = theme === Themes.light ? Themes.dark : Themes.light;
         setTheme(newTheme);
         changeTheme(newTheme);
+    };
+
+    const handleDropdownChange = (e: DropdownChangeEvent) => {
+        setDropdowntheme(e.value);
+        changeTheme(`/themes/${e.value}/theme.css`);
     };
 
     return (
@@ -34,11 +53,17 @@ export const Header = () => {
                         <NavLink to="/">
                             <div className={classes.logo}>PProjects.</div>
                         </NavLink>
-                        <InputSwitch
+                        {/* <InputSwitch
                             checked={isLight}
                             onChange={handleOnThemeChange}
                             className={classes.themeSwithcer}
-                        ></InputSwitch>
+                        ></InputSwitch> */}
+                        <Dropdown
+                            className={classes.Dropdown}
+                            onChange={handleDropdownChange}
+                            options={DropdownThemes}
+                            value={dropdowntheme}
+                        />
                     </div>
                 </div>
             </div>
